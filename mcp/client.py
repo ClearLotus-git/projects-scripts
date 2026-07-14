ccd864b6f7b955694fb0f16495300a73eb46bd38
ClearLotus-git <71709864+ClearLotus-git@users.noreply.github.com>
2026-07-14 10:24:12 -0400

    Refactor client to use resources and templates
    functions list_resources(), list_resource_templates(), and read_resource()

--- mcp/client.py
+++ mcp/client.py
@@ -5,10 +5,16 @@ client = Client("http://localhost:8000/mcp/")
 
 async def main():
     async with client:
-        tools = await client.list_tools()
-        result_object = await client.call_tool("store_file", {"file_content": "Hello World!", "file_name": "helloworld"})
-        result_text = result_object.content[0].text
+        resources = await client.list_resources()
+        result_object = await client.read_resource("resource://filecount")
+        result_text = result_object[0].text
 
-        print(f"*** Available Tools:\n{tools}\n*** Tool Result:\n{result_text}\n")
+        print(f"*** Available Resources:\n{resources}\n*** Resource Result:\n{result_text}\n")
+
+        resource_templates = await client.list_resource_templates()
+        result_object = await client.read_resource("getfile://helloworld")
+        result_text = result_object[0].text
+
+        print(f"*** Available Resource Templates:\n{resource_templates}\n*** Resource Template Result:\n{result_text}\n")
 
 asyncio.run(main())
